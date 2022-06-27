c528a7d96ad74bbe1819fc3ecad0f9b06c3de2a1
sonjipk <102653285+sonjipk@users.noreply.github.com>
2022-06-27 20:10:59 +0200

    Create listaelementos.js

---
+++ JavaScript-Básico/02 - Sintaxis, variables y palabras/listaelementos.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Sonia",
+    33,
+    true,
+    new Date(1989, 03, 22),
+    { 
+        titulo: "El mundo de Sofía",
+        autor: "JOSTEIN GAARDER",
+        fecha: new Date(1994, 0, 1),
+        url: "https://www.casadellibro.com/libro-el-mundo-de-sofia/9788498414516/1726492"
+      },
+  ];
